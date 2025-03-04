0433629bc6c1b99ce92c843b1348ff82d0818b28
roger <rogerprimiani@gmail.com>
2025-03-03 23:45:34 -0300

    FrontEnd: MudBlazor

---
+++ AppPrimiani.Web/Configuration.cs
@@ -0,0 +1,23 @@
+﻿using MudBlazor;
+
+namespace AppPrimiani.Web
+{
+    public static class Configuration
+    {
+        public static MudTheme Theme = new MudTheme()
+        {
+            PaletteLight = new PaletteLight()
+            {
+                Primary = "#3f51b5",
+                Secondary = "#f50057",
+                Background = "#f5f5f5",
+            },
+            PaletteDark = new PaletteDark()
+            {
+                Primary = "#3f51b5",
+                Secondary = "#f50057",
+                Background = "#333",
+            }
+        };
+    }
+}
